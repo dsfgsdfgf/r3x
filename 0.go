809c29456aae2314682d0f515b52b46e3c3d8772
dsfgsdfgf <kaanixir@gmail.com>
2017-06-14 17:38:38 +1000

    Websocket Upgrade

--- 0.go
+++ 0.go
@@ -11,10 +11,17 @@ import (
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+
 	"fmt"
 )
 
-var counter int
+var counter int     					// Global Counter <- The Order
+var tickcounter int 					// Global Counter <- AllTix
+var upgrader = websocket.Upgrader{		// Websocket API v0.1
+	ReadBufferSize:  64,				//  Default: 1024 ?
+	WriteBufferSize: 64,				//  Default: 1024 ?
+}
 
 func main() {
 	var entry string
@@ -26,28 +33,27 @@ func main() {
 	flag.StringVar(&port, "port", "8000", "R3X Server Port.")
 	flag.Parse()
 
+	// <<< ROUTE >>>
 	r := mux.NewRouter()
-
-	api := r.PathPrefix("/api/v1/").Subrouter()
-	api.HandleFunc("/ticker", TickerHandler).Methods("GET")		// Ticker BTC - AUD
-	api.HandleFunc("/buy", BuyHandler).Methods("POST")			// BTC Purchase
-
+	// /api/v1/... production mindset etc...
+	api := r.PathPrefix("/api/").Subrouter()
+	api.HandleFunc("/ticker", TickerHandler).Methods("GET") // Ticker BTC - AUD
+	api.HandleFunc("/buy", BuyHandler).Methods("POST")      // BTC Purchase
 	// static files folder css/js...
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir(static)))
 	// 		*** not needed when static assets  ==  "/"
 	// JavaScript entry-point (index.html).
 	//r.PathPrefix("/index.html").HandlerFunc(IndexHandler(entry))
 
+	// < < SERVE > >
 	srv := &http.Server{
-		Handler: handlers.LoggingHandler(os.Stdout, r),
-		Addr:    "127.0.0.1:" + port,
+		Handler:      handlers.LoggingHandler(os.Stdout, r),
+		Addr:         "127.0.0.1:" + port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-
 	// Todo: 	Official Cert - ListenAndServeTLS(...)
 	// https://gist.github.com/d-schmidt/587ceec34ce1334a5e60
-
 	log.Fatal(srv.ListenAndServe())
 }
 
@@ -59,12 +65,32 @@ func IndexHandler(entrypoint string) func(w http.ResponseWriter, r *http.Request
 	return http.HandlerFunc(fn)
 }
 
+// UPDATE THE BTC - AUD PRICE
+func BTC(){
+
+}
+
 // >> > >> > > > > > > >> >  >>  > ** * ** * * *
 // 			BTC - AUD 		price 	ticker !  >  >    >
 // >> > >> > > > > > > >> >  >>  > ** * ** * * *
 func TickerHandler(w http.ResponseWriter, r *http.Request) {
+	// Todo: ResponseHandler pls + performance analysis for each conn... w/ ping/pong or extended JSON timeVal diff based latency
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("--> Client Websocket Upgrade [fail]")
+		log.Println("-->", err)
+		return
+	}
+
+	// Check client WebSocket Todo: ( if no WS, REST API Fallback )
+	if websocket.IsWebSocketUpgrade(r) {
+		log.Println("---> Client Websocket Upgrade [OK]")
+		log.Println("---> ", conn.LocalAddr().String())
+	} // else REST
+
+
 	data := map[string]interface{}{
-		"id": "12345",
+		"id": "12345examplebro123",
 		"ts": time.Now().Format(time.RFC3339),
 	}
 
@@ -74,21 +100,21 @@ func TickerHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write(b)
+	conn.WriteJSON(b)
+	//w.Write(b)
 }
-
 
 // ===================================
 // ===================================
 // ! ! ! 	BITCOIN ORDER 		 ! ! !
 // ===================================
 func BuyHandler(w http.ResponseWriter, r *http.Request) {
-	amount := r.PostForm.Get("btc")		// Amount requested !
+	amount := r.PostForm.Get("btc") // Amount requested !
 
 	data := map[string]interface{}{
-		"id": fmt.Sprintf("%d", counter),
+		"id":  fmt.Sprintf("%d", counter),
 		"btc": amount,
-		"ts": time.Now().Format(time.RFC3339),
+		"ts":  time.Now().Format(time.RFC3339),
 	}
 
 	b, err := json.Marshal(data)
@@ -97,7 +123,6 @@ func BuyHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-
-	counter++			// The Order Counter
+	counter++ // The Order Counter
 	w.Write(b)
 }
